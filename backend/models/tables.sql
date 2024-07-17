c0f33853e0a163d44d1bb327bf338778c1bc561a
koacow <khoa.cnd@gmail.com>
2024-07-17 14:11:15 +1000

    finally figured out database schema

--- backend/models/tables.sql
+++ backend/models/tables.sql
@@ -1,7 +1,5 @@
 --- SQL commands to create the tables in the database
 
---- Users tables
-
 CREATE TABLE IF NOT EXISTS user_profiles (
     user_id UUID REFERENCES auth.users ON DELETE CASCADE,
     username VARCHAR(255) NOT NULL,
@@ -17,40 +15,25 @@ CREATE TABLE IF NOT EXISTS user_settings (
     PRIMARY KEY (user_id)
 );
 
-
-CREATE TABLE IF NOT EXISTS keywords (
+CREATE TABLE IF NOT EXISTS topics (
     id SERIAL PRIMARY KEY,
-    keyword VARCHAR(255) NOT NULL UNIQUE
+    topic VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS saved_articles (
+CREATE TABLE IF NOT EXISTS user_topics (
     user_id UUID REFERENCES auth.users ON DELETE CASCADE,
-    article_url REFERENCES articles(id) ON DELETE CASCADE,
-    PRIMARY KEY (user_id, article_id)
-);
-
-CREATE TABLE IF NOT EXISTS user_keywords (
-    user_id UUID REFERENCES auth.users ON DELETE CASCADE,
-    keyword_id INT REFERENCES keywords(id) ON DELETE CASCADE,
+    topic_id INT REFERENCES topics(id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, keyword_id)
-);
-
---- Stocks tables
-CREATE TABLE IF NOT EXISTS stocks (
-    id UUID PRIMARY KEY,
-    ticker VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS saved_stocks (
     user_id UUID REFERENCES auth.users ON DELETE CASCADE,
-    stock_id UUID REFERENCES stocks(id) ON DELETE CASCADE,
-    PRIMARY KEY (user_id, stock_ticker)
+    stock_ticker VARCHAR(10) NOT NULL,
+    PRIMARY KEY (user_id, stock_id)
 );
 
 ALTER TABLE user_profiles ENABLE ROW LEVEL SECURITY;
 ALTER TABLE user_settings ENABLE ROW LEVEL SECURITY;
-ALTER TABLE saved_articles ENABLE ROW LEVEL SECURITY;
 ALTER TABLE saved_stocks ENABLE ROW LEVEL SECURITY;
-ALTER TABLE user_keywords ENABLE ROW LEVEL SECURITY;
+ALTER TABLE user_topics ENABLE ROW LEVEL SECURITY;
 
