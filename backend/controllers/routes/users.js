d481cf08fd106f35b2639ed74484f4780d401bd9
koacow <khoa.cnd@gmail.com>
2024-07-24 12:20:01 +1000

    added input validation to /users routes

--- backend/controllers/routes/users.js
+++ backend/controllers/routes/users.js
@@ -1,60 +1,82 @@
 // usersRouter matches /api/users paths
-
+const { body, validationResult } = require('express-validator');
 const usersRouter = require('express').Router();
 const supabase = require('../../models/db');
 
-usersRouter.post('/authenticate', async (req, res) => {
-    const { email, password } = req.body;
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-        options: {
-            emailRedirectTo: 'http://localhost:5173',
-        },
-    });
-    if (error) {
-        return res.status(400).json({ 
-            error: error.message,
-            code: error.code
-         });
-    }
-    return res.status(200).json(data);
-});
-
-usersRouter.post('/register', async (req, res) => {
-    const { email, password, first_name, last_name } = req.body;
-    const { data: authData, error: authError } = await supabase.auth.signUp({
-        email,
-        password
-    });
-    if (authError) {
-        return res.status(400).json({ 
-            error: authError.message,
-            code: authError.code
-         });
-    }
-    const user_id = authData.user.id;
-    const { error: insertError } = await supabase.from('user_profiles').insert({ user_id, first_name, last_name, region_code });
-    if (insertError) {
-        return res.status(400).json({ 
-            error: userError.message,
-            code: userError.code
+usersRouter.post('/authenticate', 
+    body('email').isEmail().normalizeEmail(),
+    body('password').isLength({ min: 6 }),
+    async (req, res) => {
+        const validationErrors = validationResult(req);
+        if (!validationErrors.isEmpty()) {
+            return res.status(400).json({ error: 'Invalid input' });
+        }
+        const { email, password } = req.body;
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+            options: {
+                emailRedirectTo: 'http://localhost:5173',
+            },
         });
-    }
-    return res.status(200).json(authData);
+        if (error) {
+            return res.status(400).json({ 
+                error: error.message,
+                code: error.code
+            });
+        }
+        return res.status(200).json(data);
 });
 
-usersRouter.put('/reset-password', async (req, res) => {
-    const { email } = req.body;
-    const { error} = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: 'http://localhost:5173',
-    });
-    if (error) {
-        return res.status(400).json({ 
-            error: error.message,
-            code: error.code
-         });
-    }
-    return res.status(200).json({ message: 'Password reset email sent' });
+usersRouter.post('/register',
+    body('email').isEmail().normalizeEmail(),
+    body('password').isLength({ min: 6 }),
+    body('first_name').notEmpty().isString().escape(),
+    body('last_name').notEmpty().isString().escape(), 
+    async (req, res) => {
+        const validationErrors = validationResult(req);
+        if (!validationErrors.isEmpty()) {
+            return res.status(400).json({ error: 'Invalid input' });
+        }
+        const { email, password, first_name, last_name } = req.body;
+        const { data: authData, error: authError } = await supabase.auth.signUp({
+            email,
+            password
+        });
+        if (authError) {
+            return res.status(400).json({ 
+                error: authError.message,
+                code: authError.code
+            });
+        }
+        const user_id = authData.user.id;
+        const { error: insertError } = await supabase.from('user_profiles').insert({ user_id, first_name, last_name, region_code });
+        if (insertError) {
+            return res.status(400).json({ 
+                error: userError.message,
+                code: userError.code
+            });
+        }
+        return res.status(200).json(authData);
+});
+
+usersRouter.put('/reset-password',
+    body('email').isEmail().normalize, 
+    async (req, res) => {
+        const validationErrors = validationResult(req);
+        if (!validationErrors.isEmpty()) {
+            return res.status(400).json({ error: 'Invalid input' });
+        }
+        const { email } = req.body;
+        const { error} = await supabase.auth.resetPasswordForEmail(email, {
+            redirectTo: 'http://localhost:5173',
+        });
+        if (error) {
+            return res.status(400).json({ 
+                error: error.message,
+                code: error.code
+            });
+        }
+        return res.status(200).json({ message: 'Password reset email sent' });
 });
 module.exports = usersRouter;
