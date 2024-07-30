d2d7b42dd7b7e724f4917913e7ce548fbf80bd3e
koacow <khoa.cnd@gmail.com>
2024-07-30 12:13:41 +1000

    added handling of different status codes to accountAPI functions

--- frontend/src/api/accountAPI.js
+++ frontend/src/api/accountAPI.js
@@ -3,7 +3,14 @@ const ENDPOINT = '/api/account';
 export default accountAPI = {
     getUserSettings: async () => {
         const response = await fetch(`${ENDPOINT}/settings`);
-        return await response.json();
+        switch (response.status) {
+            case 200:
+                return response.json();
+            case 404:
+                return defaultSettings;
+            default:
+                throw new Error('Failed to fetch user settings');
+        }
     },
     createUserSettingsProfile: async (user_id) => {
         const response = await fetch(`${ENDPOINT}/settings?user_id=${encodeURIComponent(user_id)}`, {
@@ -13,7 +20,12 @@ export default accountAPI = {
             },
             body: JSON.stringify(defaultSettings)
         });
-        return await response.json();
+        switch (response.status) {
+            case 201:
+                return response.json();
+            default:
+                throw new Error('Failed to create user settings profile');
+        }
     },
     updateUserSettings: async (user_id, settings) => {
         const response = await fetch(`${ENDPOINT}/settings?user_id=${encodeURIComponent(user_id)}`, {
@@ -23,17 +35,32 @@ export default accountAPI = {
             },
             body: JSON.stringify(settings)
         });
-        return await response.json();
+        switch (response.status) {
+            case 200:
+                return response.json();
+            default:
+                throw new Error('Failed to update user settings');
+        }
     },
     resetUserSettings: async (user_id) => {
         const response = await fetch(`${ENDPOINT}/settings?user_id=${encodeURIComponent(user_id)}`, {
             method: 'DELETE'
         });
-        return await response.json();
+        switch(response.status) {
+            case 200:
+                return;
+            default:
+                throw new Error('Failed to reset user settings');
+        }
     },
     getUserSavedTopics: async (user_id) => {
         const response = await fetch(`${ENDPOINT}/topics?user_id=${encodeURIComponent(user_id)}`);
-        return await response.json();
+        switch (response.status) {
+            case 200:
+                return response.json();
+            default:
+                throw new Error('Failed to fetch user topics');
+        }
     },
     updateUserSavedTopics: async (user_id, topic_ids) => {
         const response = await fetch(`${ENDPOINT}/topics?user_id=${encodeURIComponent(user_id)}`, {
@@ -43,17 +70,32 @@ export default accountAPI = {
             },
             body: JSON.stringify({ topic_ids })
         });
-        return await response.json();
+        switch (response.status) {
+            case 201:
+                return response.json();
+            default:
+                throw new Error('Failed to update user topics');
+        }
     },
     deleteUserSavedTopics: async (user_id) => {
         const response = await fetch(`${ENDPOINT}/topics?user_id=${encodeURIComponent(user_id)}`, {
             method: 'DELETE'
         });
-        return await response.json();
+        switch(response.status) {
+            case 204:
+                return;
+            default:
+                throw new Error('Failed to delete user topics');
+        }
     },
     getUserSavedStocks: async (user_id) => {
         const response = await fetch(`${ENDPOINT}/stocks?user_id=${encodeURIComponent(user_id)}`);
-        return await response.json();
+        switch(response.status){
+            case 200:
+                return response.json();
+            default:
+                throw new Error('Failed to fetch user saved stocks');
+        }
     },
     updateUserSavedStocks: async (user_id, stock_tickers) => {
         const response = await fetch(`${ENDPOINT}/stocks?user_id=${encodeURIComponent(user_id)}`, {
@@ -63,12 +105,22 @@ export default accountAPI = {
             },
             body: JSON.stringify({ stock_tickers })
         });
-        return await response.json();
+        switch(response.status){
+            case 201:
+                return response.json();
+            default:
+                throw new Error('Failed to update user saved stocks');
+        }
     },
     deleteUserSavedStocks: async (user_id) => {
         const response = await fetch(`${ENDPOINT}/stocks?user_id=${encodeURIComponent(user_id)}`, {
             method: 'DELETE'
         });
-        return await response.json();
+        switch(response.status){
+            case 204:
+                return;
+            default:
+                throw new Error('Failed to delete user saved stocks');
+        }
     }
 };
