3adb8032285fc6fe2275c1486b2e3e039b51fe94
koacow <khoa.cnd@gmail.com>
2024-07-30 11:56:41 +1000

    created settingsSlice

--- frontend/src/state/slices/searchSlice.js
+++ frontend/src/state/slices/searchSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { getArticlesByQuery, getAllTopics, getArticlesByTopic } from '../../api/articlesAPI';
+import { getArticlesByQuery } from '../../api/articlesAPI';
 
 const initialState = {
     query: '',
---
+++ frontend/src/state/slices/settingsSlice.js
@@ -0,0 +1,132 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getSettings, updateUserSettings, resetUserSettings } from '../../api/accountAPI';
+
+const initialState = {
+	display: {
+		darkMode: false,
+		language: "en"
+	},
+	notifications: {
+		email: {
+			dailyDigest: true,
+			newsletter: true
+		},
+		push: {
+			dailyDigest: true,
+			newsletter: true
+		}
+	},
+    loading: false,
+    error: null,
+};
+
+const settingsSlice = createSlice({
+    name: 'settings',
+    initialState,
+    reducers: {
+        setLocalSettings(state, action) {
+            state.display = action.payload.display;
+            state.notifications = action.payload.notifications;
+        },
+        setLocalDarkMode(state, action) {
+            state.display.darkMode = action.payload;
+        },
+        setLocalLanguage(state, action) {
+            state.display.language = action.payload;
+        },
+        setLocalEmailNotifications(state, action) {
+            state.notifications.email = action.payload;
+        },
+        setLocalPushNotifications(state, action) {
+            state.notifications.push = action.payload;
+        },
+    },
+    // Async reducers assume that the user is already logged in
+    extraReducers: {
+        [fetchSettingsAsync.fulfilled]: (state, action) => {
+            state.display = action.payload.display;
+            state.notifications = action.payload.notifications;
+            state.loading = false;
+            state.error = null;
+        },
+        [fetchSettingsAsync.pending]: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        [fetchSettingsAsync.rejected]: (state, action) => {
+            state.error = action.payload.error;
+            state.loading = false;
+        },
+        [updateSettingsAsync.fulfilled]: (state, action) => {
+            state.display = action.payload.display;
+            state.notifications = action.payload.notifications;
+            state.loading = false;
+            state.error = null;
+        },
+        [updateSettingsAsync.pending]: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        [updateSettingsAsync.rejected]: (state, action) => {
+            state.error = action.payload.error;
+            state.loading = false;
+        },
+        [resetSettingsAsync.fulfilled]: (state, action) => {
+            state.display = action.payload.display;
+            state.notifications = action.payload.notifications;
+            state.loading = false;
+            state.error = null;
+        },
+        [resetSettingsAsync.pending]: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        [resetSettingsAsync.rejected]: (state, action) => {
+            state.error = action.payload.error;
+            state.loading = false;
+        },
+    }
+});
+
+
+export const fetchSettingsAsync = createAsyncThunk(
+    'settings/fetchSettingsAsync',
+    async (_, thunkAPI) => {
+        try {
+            const userId = thunkAPI.getState().session.data.user.id;
+            const response = await getSettings(userId);
+            return response;
+        } catch (error) {
+            return thunkAPI.rejectWithValue({ error: error.message });
+        }
+    }
+);
+
+export const updateSettingsAsync = createAsyncThunk(
+    'settings/updateSettingsAsync',
+    async (settings, thunkAPI) => {
+        try {
+            const userId = thunkAPI.getState().session.data.user.id;
+            await updateUserSettings(userId, settings);
+            return settings;
+        } catch (error) {
+            return thunkAPI.rejectWithValue({ error: error.message });
+        }
+    }
+);
+
+export const resetSettingsAsync = createAsyncThunk(
+    'settings/resetSettingsAsync',
+    async (_, thunkAPI) => {
+        try {
+            const userId = thunkAPI.getState().session.data.user.id;
+            await resetUserSettings(userId);
+            return initialState;
+        } catch (error) {
+            return thunkAPI.rejectWithValue({ error: error.message });
+        }
+    }
+);
+
+export const { setLocalSettings, setLocalDarkMode, setLocalLanguage, setLocalEmailNotifications, setLocalPushNotifications } = settingsSlice.actions;
+export default settingsSlice.reducer;
