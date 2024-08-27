bddfead5592acb705f90c8211092afd27305ea23
koacow <khoa.cnd@gmail.com>
2024-08-27 16:19:27 +1000

    updated sizing and positioning for article card error element

--- frontend/src/components/ArticleCard/ArticleCardError.jsx
+++ frontend/src/components/ArticleCard/ArticleCardError.jsx
@@ -5,9 +5,10 @@ import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 
 export default function ArticleCardError() {
     return (
-        <Card className='flex flex-col justify-center items-center'>
+        <Card className='flex flex-col justify-center items-center text-center'>
             <CardHeader
-                avatar={<WarningAmberIcon color='error' />}
+                className='flex flex-col md:flex-row'
+                avatar={<WarningAmberIcon color='error' className='size-11 md:size-6' />}
                 title={<Typography variant='h6' component='h3' color='error'>
                     Something went wrong when loading these articles. Try again later.
                 </Typography>}
