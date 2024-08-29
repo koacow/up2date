19315ea9e1fb4487dbbc504cd7f1e121373018a5
koacow <khoa.cnd@gmail.com>
2024-08-29 12:31:06 +1000

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+export default function Footer () {
+    return (
+        <AppBar color='primary' className='flex fixed bottom-0 top-auto justify-center p-5'>
+            <Container>
+                <Typography variant='body1' className='text-center'>
+                    &copy; 2024 Up2Date
+                </Typography>
+            </Container>
+        </AppBar>
+    )
+}
--- src/pages/MainTemplate.jsx
+++ src/pages/MainTemplate.jsx
@@ -1,4 +1,5 @@
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -30,6 +31,7 @@ export default function MainTemplate() {
         <>
             <NavBar />
             <Outlet />
+            <Footer />
         </>
     )
 }
