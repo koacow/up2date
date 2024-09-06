45c44ff13e2f7e45b91a49eeb6f2550892fd38f4
koacow <khoa.cnd@gmail.com>
2024-09-06 13:00:25 -0400

    tiny tweak to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 export default function Footer () {
     return (
-        <AppBar color='primary' className='flex fixed bottom-0 top-auto justify-center p-5'>
+        <AppBar color='primary' className='flex fixed bottom-0 top-auto h-16 justify-center p-5'>
             <Container>
                 <Typography variant='body1' className='text-center'>
                     &copy; 2024 Up2Date
