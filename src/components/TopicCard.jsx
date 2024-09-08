db9876777fa59aebe94bb4585f720a3dacaeb1d5
koacow <khoa.cnd@gmail.com>
2024-09-08 17:41:40 -0400

    changed topic card to expand instead of scroll

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -3,19 +3,24 @@ import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Pagination from "@mui/material/Pagination";
 import Divider from "@mui/material/Divider";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import ArticleCard from "./ArticleCard/ArticleCard";
+import ArticleCardLoading from "./ArticleCard/ArticleCardLoading";
+import ArticleCardError from "./ArticleCard/ArticleCardError";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPageNumForTopic } from "../state/slices/topicsSlice";
 import { fetchArticlesForSavedTopic } from "../state/slices/articlesSlice";
-import ArticleCard from "./ArticleCard/ArticleCard";
-import ArticleCardLoading from "./ArticleCard/ArticleCardLoading";
-import ArticleCardError from "./ArticleCard/ArticleCardError";
 
 export default function TopicCard({ topic }) {
     const session = useSelector((state) => state.session.session);
     const dispatch = useDispatch();
     const { id, topic: topicName, pageNum } = topic;
     const [paginationDisplayedNum, setPaginationDisplayedNum] = useState(pageNum);
+    const [ topicCardExpanded, setTopicCardExpanded ] = useState(false);
+    const [ expandMoreRotation, setExpandMoreRotation ] = useState(0);
 
     useEffect(() => {
         dispatch(fetchArticlesForSavedTopic(topic));
@@ -29,9 +34,14 @@ export default function TopicCard({ topic }) {
         setPaginationDisplayedNum(value);
     }
 
+    const handleToggleTopicCardExpanded = () => {
+        setTopicCardExpanded(prev => !prev);
+        setExpandMoreRotation(topicCardExpanded ? 0 : 180);
+    }
+
     const renderArticles = () => {
         if (loading) {
-            return [...Array(4)].map((_, index) => {
+            return [...Array(10)].map((_, index) => {
                 return <ArticleCardLoading key={index} />
             })
         } else if (error) {
@@ -62,16 +72,20 @@ export default function TopicCard({ topic }) {
     }
 
     return (
-        <Box component='section' className='shadow-lg rounded-md md:h-screen md:overflow-y-scroll  my-5 pb-5 relative' >
+        <Box component='section' className='shadow-lg rounded-md my-5 pb-5 relative' >
             <Typography 
                 component='h1' 
                 variant='h1'
-                className='tracking-wider font-semibold md:text-center m-4'
+                className='tracking-wider font-semibold md:text-center m-4 cursor-pointer'
                 fontFamily={ 'Merriweather, serif' }
+                onClick={handleToggleTopicCardExpanded} 
             >
-                {topicName}
+                {topicName} 
+                <ExpandMore className={`absolute right-5 top-5 transform rotate-${expandMoreRotation} transition-transform duration-300`} />
             </Typography>
-            {renderArticles()}
+            <Collapse in={topicCardExpanded} unmountOnExit>
+                {renderArticles()}
+            </Collapse>
         </Box>
     )
 }
