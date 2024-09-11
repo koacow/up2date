1faee50edd02fd1e7f02b23c030d43c068ee4ab7
koacow <khoa.cnd@gmail.com>
2024-09-11 13:11:02 -0400

    created top-headlines branch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 
 export default function Home() {
     const savedTopics = useSelector((state) => state.topics.topics);
-      
 
     return (
         <>
